294972810350d01e0acea4b5529417c2b092ebc1
qeaml <qeaml@proton.me>
2023-07-29 17:34:24 +0200

    Run commands thru shell in Compiler.build_exe

--- bip/compiler.py
+++ bip/compiler.py
@@ -124,7 +124,7 @@ class GNULike(Compiler):
       flags.append(f"-l{l}")
     cmd = self._cmd + " " + " ".join(flags)
     inf.log.verbose(cmd)
-    return subprocess.run(cmd).returncode == 0
+    return subprocess.run(cmd, shell=True).returncode == 0
     # return True
 
   def build_lib(self, inf: Info, objs: list[Path], out: Path) -> bool:
